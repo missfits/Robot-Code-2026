9e0181cc80f7a6c0b14e63504466b2349ac42a06
tori <144298063+tortillini-tokki@users.noreply.github.com>
2026-01-20 22:14:57 -0800

    refactor LocalizationCamera vision class
    IMPORTANT CHANGES:
    - removed timestamp converter when performing addVisionMeasurement to drivetrain (removed for 2026 season)
    - changed vision measurement acceptance criteria: EITHER individual camera pose is consistent for last 3 readings OR distance between estimated vision pose and actual pose is less than max distance. This helpes mitigate our crash-into-walls-can't-recover issue by elimating the cooldown period between odometry messing up and vision attempting to correct.
    
    OTHER CHANGES:
    - added logging statements for each characteristic of a CameraReading + filtering checks
    - added PosePublisher (2D and 3D) to publish estimatedRobotPose to NetworkTables
    - rename isEstPoseJumpy() to areRecentCameraPosesConsistent() as clearer method name (now returns TRUE if last 3 frames are consistent, FALSE if not)

--- src/main/java/frc/robot/subsystems/vision/LocalizationCamera.java
+++ src/main/java/frc/robot/subsystems/vision/LocalizationCamera.java
@@ -5,9 +5,12 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -36,13 +39,23 @@ public class LocalizationCamera {
 
   private Optional<CameraReading> m_currentReading = Optional.empty();
 
-  public static record CameraReading(EstimatedRobotPose robotPose, Matrix<N3, N1> stdDevs, double timestampSeconds, Integer numTargets) {}
+  private final StructPublisher<Pose2d> pose2dPublisher;
+  private final StructPublisher<Pose3d> pose3dPublisher;
+
+  // every camera periodically creates a new CameraReading containing robot pose, std dev, timestamp, and number of targets seen.
+  public static record CameraReading(EstimatedRobotPose robotPose, Matrix<N3, N1> stdDevs, double timestampSeconds, int numTargets) {}
 
   public LocalizationCamera(String cameraName, Transform3d robotToCam) {
     m_cameraName = cameraName;
     m_camera = new PhotonCamera(m_cameraName);
     m_logString = "vision/" + m_cameraName;
     poseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCam);
+
+    pose2dPublisher = NetworkTableInstance.getDefault()
+            .getStructTopic("SmartDashboard/" + m_logString + "/estimatedRobotPose2D", Pose2d.struct).publish();
+
+    pose3dPublisher = NetworkTableInstance.getDefault()
+            .getStructTopic("SmartDashboard/" + m_logString + "/estimatedRobotPose3D", Pose3d.struct).publish();
 
     SmartDashboard.putBoolean("isConnected/" + m_cameraName, m_camera.isConnected());
   }
@@ -78,11 +91,24 @@ public class LocalizationCamera {
         m_lastReadings.removeFirst();
       }
 
-      SmartDashboard.putBoolean(m_logString + "/is-Present", true);
-      SmartDashboard.putString(m_logString + "/is-multiTag", newReading.get().numTargets() > 1 ? "multitagReading" : "singleTagReading");
+      // logging all cameraReading data to SmartDashboard
+      SmartDashboard.putBoolean(m_logString + "/reading-is-Present", true);
+
+      SmartDashboard.putNumber(m_logString + "/reading-num-targets-seen", newReading.get().numTargets());
+      SmartDashboard.putNumberArray(m_logString + "/reading-standard-devs", newReading.get().stdDevs().getData());
+      SmartDashboard.putNumber(m_logString + "/reading-timestamp", newReading.get().timestampSeconds());
+      SmartDashboard.putString(m_logString + "/reading-is-multiTag", newReading.get().numTargets() > 1 ? "multitagReading" : "singleTagReading");
+
     } else {
-      SmartDashboard.putBoolean(m_logString + "/is-Present", false);
+      SmartDashboard.putBoolean(m_logString + "/reading-is-Present", false);
     }
+    // no matter what, want to publish isConnected to NetworkTables
+    SmartDashboard.putBoolean("isConnected/" + m_cameraName, m_camera.isConnected());
+
+    // publish estimated robot Pose2d to NetworkTables
+    // uses map for efficient unwrapping of Optional<CameraReading>
+    pose2dPublisher.set(newReading.map(reading -> reading.robotPose().estimatedPose.toPose2d()).orElse(null));
+    pose3dPublisher.set(newReading.map(reading -> reading.robotPose().estimatedPose).orElse(null));
   }
 
   private Optional<CameraReading> calculateNewCameraReading() {
@@ -147,11 +173,14 @@ public class LocalizationCamera {
     }
   }
 
-  // checks if the pose is jumpy based on avg speed since by calculating based on speed, 
-  // the camera fps doesn't matter as the speed between readings will still be the same. this is based on last 3 readings
-  public boolean isEstPoseJumpy() {
+  /*
+   * returns true if the average speed between the last 3 camera readings (FROM ONE CAMREA)
+   * is less than the max average speed.
+   * goal is to check if the last three readings are smooth + consistent. 
+   */
+  public boolean areRecentCameraPosesConsistent() {
     if (m_lastReadings.size() < VisionConstants.NUM_LAST_EST_POSES) {
-      return true;
+      return false;
     }
 
     double totalDistance = 0;
@@ -170,7 +199,7 @@ public class LocalizationCamera {
     double avgDist = totalDistance / (m_lastReadings.size() - 1);
     double avgTime = totalTime / (m_lastReadings.size() - 1);
     if (avgTime == 0){
-      return true;
+      return false;
     }
     double avgSpeed = avgDist/avgTime;
 
@@ -178,7 +207,7 @@ public class LocalizationCamera {
     SmartDashboard.putNumber("vision/" + m_cameraName + "/avgSpeedBetweenLastEstPoses", avgSpeed);
     SmartDashboard.putNumber("vision/" + m_cameraName + "/avgTimeBetweenLastEstPoses", avgTime);
 
-    return avgSpeed > VisionConstants.MAX_AVG_SPEED_BETWEEN_LAST_EST_POSES;
+    return avgSpeed < VisionConstants.MAX_AVG_SPEED_BETWEEN_LAST_EST_POSES;
   }
 }
 
