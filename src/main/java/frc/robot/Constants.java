5bffb66999fa0a93b474a1defe5ad2b974b72853
zoe <zoe.z.zhao@gmail.com>
2026-01-20 21:23:01 -0800

    fix merge from main

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -60,9 +60,9 @@ public final class Constants {
     public static final double MAX_ROTATION_SPEED = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     // Rotation heading controller PID gains
-    public static final double ROTATION_KP = 5.0;
-    public static final double ROTATION_KI = 0.0;
-    public static final double ROTATION_KD = 0.0;
+    public static double ROTATION_KP = 5.0;
+    public static double ROTATION_KI = 0.0;
+    public static double ROTATION_KD = 0.0;
 
     // PID constants for PathPlanner AutoBuilder
     public static double ROBOT_POSITION_P = 5.0;
