b6a8bc951c0bd5449601ff0e45a8aee6ba0d7e9a
aishahnazar <aishahnazar2@gmail.com>
2026-01-22 17:30:32 -0800

    give motor IDs high constants
    update motor ID constants from zero to numbers 50-54 so they don't conflict

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,14 +64,14 @@ public final class Constants {
 
   //INTAKE
   public static class PivotConstants {
-    public static final int MECHANISM_MOTOR_ID = 0;
+    public static final int MECHANISM_MOTOR_ID = 50;
     public static final int MOTOR_STATOR_LIMIT = 0;
 
     public static final double DEGREES_PER_ROTATION = 0;
   }
 
   public static class RollerConstants {
-    public static final int MECHANISM_MOTOR_ID = 0;
+    public static final int MECHANISM_MOTOR_ID = 51;
     public static final int MOTOR_STATOR_LIMIT = 0;
 
     public static final double DEGREES_PER_ROTATION = 0;
@@ -79,14 +79,14 @@ public final class Constants {
 
   //SCORER
   public static class IndexerConstants {
-    public static final int MECHANISM_MOTOR_ID = 0;
+    public static final int MECHANISM_MOTOR_ID = 52;
     public static final int MOTOR_STATOR_LIMIT = 0;
 
     public static final double DEGREES_PER_ROTATION = 0;
   }
 
   public static class ShooterConstants {
-    public static final int MECHANISM_MOTOR_ID = 0;
+    public static final int MECHANISM_MOTOR_ID = 53;
     public static final int MOTOR_STATOR_LIMIT = 0;
 
     public static final double DEGREES_PER_ROTATION = 0;
@@ -94,7 +94,7 @@ public final class Constants {
 
   //CLIMBER
   public static class ClimberConstants {
-    public static final int MECHANISM_MOTOR_ID = 0;
+    public static final int MECHANISM_MOTOR_ID = 54;
     public static final int MOTOR_STATOR_LIMIT = 0;
 
     public static final double METERS_PER_ROTATION = 0;
