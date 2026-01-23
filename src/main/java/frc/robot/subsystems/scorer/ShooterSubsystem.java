00f63bf614995f3388d4c70ee6846186666eac3c
aishahnazar <aishahnazar2@gmail.com>
2026-01-23 15:01:23 -0800

    update MechanismsSubsystemBase
    - add new methods to MechanismsSubsystemBase
    - update ShooterSubsystem so it extends MechanismsSubsystemBase
    - (also renamed MissfitsSubsystemBase to MechanismsSubsystemBase)

--- src/main/java/frc/robot/subsystems/scorer/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/scorer/ShooterSubsystem.java
@@ -2,63 +2,30 @@ package frc.robot.subsystems.scorer;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.MechanismsSubsystemBase;
 
 
-public class ShooterSubsystem extends SubsystemBase {
+public class ShooterSubsystem extends MechanismsSubsystemBase {
   private final ShooterIOHardware m_IO = new ShooterIOHardware(ShooterConstants.MECHANISM_MOTOR_ID);
 
   public ShooterSubsystem() {
-        m_IO.resetPosition();
+    super("shooter", "shooter");
+    m_IO.resetPosition();
   }
 
-  public Command runShooter(double speed) {
-    return loggedCommand(
-        "runShooter",
-        this.run(() -> m_IO.setVoltage(speed))
-    );
-  }
-  public Command velocityVoltage(double velocity, boolean enableFOC, double feedForward, int slot, boolean overrideBrakeDurNeutral) {
-    VelocityVoltage request = new VelocityVoltage(velocity)
-        .withEnableFOC(enableFOC)
-        .withFeedForward(feedForward)
-        .withSlot(slot)
-        .withOverrideBrakeDurNeutral(overrideBrakeDurNeutral);
-
-    return loggedCommand(
-      "velocityVoltage",
-      this.run(() -> m_IO.setVelocityVoltage(request))
-  );
-}
-
-  public Command runShooterOff() {
-    return loggedCommand(
-      "runShooterOff",
-      new RunCommand(() -> m_IO.setVoltage(0), this)
-      );
+  protected void setVoltage(double volts) {
+    m_IO.setVoltage(volts);
   }
 
-  private Command loggedCommand(String name, Command command) {
-  return command.withName(name).beforeStarting(() ->
-          SmartDashboard.putString("shooter/lastRunningInnerCommand", name)
-      );
+  protected void applyVelocityVoltage(VelocityVoltage request) {
+    m_IO.setVelocityVoltage(request);
   }
-
 
   @Override
   public void periodic() {
-    SmartDashboard.putData("shooter/subsystem", this);
+    super.periodic();
     SmartDashboard.putNumber("shooter/current", m_IO.getCurrent());
-
-    Command current = this.getCurrentCommand();
-    SmartDashboard.putString(
-      "shooter/currentlyRunningOuterCommand",
-      current != null ? current.getName() : "None"
-  );
-}
+  }
 }
