136bbd2deef76a3e63aff4710ecc90a4d7b143f1
tori lee <144298063+tortillini-tokki@users.noreply.github.com>
2026-01-25 15:10:05 -0800

    change robot back to cleo

--- src/main/java/frc/robot/RobotConfig.java
+++ src/main/java/frc/robot/RobotConfig.java
@@ -16,8 +16,7 @@ public final class RobotConfig {
   // ========================================
   // CHANGE THIS LINE TO SWITCH ROBOTS
   // ========================================
-  // private static RobotType ROBOT = RobotType.CLEO;
-  private static RobotType ROBOT = RobotType.CERIDWEN;
+  private static RobotType ROBOT = RobotType.CLEO;
 
   /**
    * Gets the current robot type.
