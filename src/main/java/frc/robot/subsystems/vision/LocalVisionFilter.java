5ea19379c550efb1f9b195980507fd512a4435c1
tori lee <144298063+tortillini-tokki@users.noreply.github.com>
2026-01-24 18:25:22 -0800

    add GlobalVisionFilter and LocalVisionFilter interfaces

---
+++ src/main/java/frc/robot/subsystems/vision/LocalVisionFilter.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.vision;
+
+import frc.robot.subsystems.vision.LocalizationCamera.CameraReading;
+
+public interface LocalVisionFilter {
+    /**
+     * Returns true if the given camera reading is valid.
+     * @param reading The camera reading to validate.
+     * @param cam The camera that took the reading.
+     * @return True if the reading is valid.
+     * 
+     * Interface for filtering logic that is specific to one camera.
+     */
+    boolean isValid(CameraReading reading, LocalizationCamera cam)
+
+}
