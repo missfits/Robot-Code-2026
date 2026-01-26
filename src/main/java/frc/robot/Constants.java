28ebb8898c0069dd7918fdf5068b633c2d0373f2
zoe <zoe.z.zhao@gmail.com>
2026-01-25 18:25:54 -0800

    fix division

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -281,15 +281,15 @@ public final class Constants {
           CAMERA2_NAME = "left_camera";
 
           // Cleo camera positions
-          ROBOT_TO_CAM1_X = Units.inchesToMeters(21/2-1.5);
-          ROBOT_TO_CAM1_Y = Units.inchesToMeters(-33/2+5);
+          ROBOT_TO_CAM1_X = Units.inchesToMeters(21.0/2-1.5);
+          ROBOT_TO_CAM1_Y = Units.inchesToMeters(-33.0/2+5);
           ROBOT_TO_CAM1_Z = Units.inchesToMeters(9);
           ROBOT_TO_CAM1_ROLL = 0;
           ROBOT_TO_CAM1_PITCH = Units.degreesToRadians(-45);
           ROBOT_TO_CAM1_YAW = 0;
 
-          ROBOT_TO_CAM2_X = Units.inchesToMeters(21/2-1);
-          ROBOT_TO_CAM2_Y = Units.inchesToMeters(33/2-5.5);
+          ROBOT_TO_CAM2_X = Units.inchesToMeters(21.0/2-1);
+          ROBOT_TO_CAM2_Y = Units.inchesToMeters(33.0/2-5.5);
           ROBOT_TO_CAM2_Z = Units.inchesToMeters(8);
           ROBOT_TO_CAM2_ROLL = 0;
           ROBOT_TO_CAM2_PITCH = Units.degreesToRadians(-45);
