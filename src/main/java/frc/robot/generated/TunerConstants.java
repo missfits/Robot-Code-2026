ddbb98b1a7d3196fb137a5c213c31ddf9e704661
zoe <zoe.z.zhao@gmail.com>
2026-01-26 17:56:25 -0800

    fix rotation for real -- invert drive motors

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -106,8 +106,8 @@ public class TunerConstants {
     private static final double kSteerGearRatio = 21.428571428571427;
     private static final Distance kWheelRadius = Inches.of(2*Constants.DrivetrainConstants.WHEEL_RADIUS_FUDGE_FACTOR);
 
-    private static final boolean kInvertLeftSide = false;
-    private static final boolean kInvertRightSide = true;
+    private static final boolean kInvertLeftSide = true;
+    private static final boolean kInvertRightSide = false;
 
     private static final int kPigeonId = 13;
 
