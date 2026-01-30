d0f78434578d8a80acc3cd0fab5fafb0a3ce9119
ariakrishnamoorthy <aria.r.krishnamoorthy@gmail.com>
2026-01-29 19:59:18 -0800

    Remove double simulationPeriodic call
    Remaining one is in MapleSimSwervePhysics drivetrain notifier

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -123,6 +123,6 @@ public class Robot extends TimedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
-    SimulatedArena.getInstance().simulationPeriodic();
+    //SimulatedArena.getInstance().simulationPeriodic();
   }
 }
