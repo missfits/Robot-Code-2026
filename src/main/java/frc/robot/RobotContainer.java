430ab3b58fda9b9710542666ec017b55faf5517b
tori <144298063+tortillini-tokki@users.noreply.github.com>
2026-01-29 20:55:15 -0800

    create LocalCameraPoseConsistencyDistanceToFusedPoseFilter (#38)
    merge two local filters to implement OR logic (isDistanceToFusedPoseValid OR isLocalCameraPoseConsistent).
    
    Merge is required for mitigation of slow correction after vision issue.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.subsystems.vision.LocalizationCamera;
 import frc.robot.subsystems.vision.LocalizationCamera.CameraReading;
 import frc.robot.subsystems.vision.VisionSubsystem;
 import frc.robot.subsystems.vision.filtering.GlobalCrossCameraConsensusFilter;
+import frc.robot.subsystems.vision.filtering.LocalCameraPoseConsistencyDistanceToFusedPoseFilter;
 import frc.robot.subsystems.vision.filtering.LocalCameraPoseConsistencyFilter;
 import frc.robot.subsystems.vision.filtering.LocalDistanceToFusedPoseFilter;
 import frc.robot.subsystems.vision.filtering.LocalPoseZRollPitchFilter;
@@ -139,8 +140,7 @@ public class RobotContainer {
 
      // Add local filters
     localPipeline.addFilter("poseZRollPitch", new LocalPoseZRollPitchFilter());
-    localPipeline.addFilter("cameraPoseConsistency", new LocalCameraPoseConsistencyFilter());
-    localPipeline.addFilter("distanceToFusedPose", new LocalDistanceToFusedPoseFilter(m_drivetrain));
+    localPipeline.addFilter("LocalCameraPoseConsistencyDistanceToFusedPose", new LocalCameraPoseConsistencyDistanceToFusedPoseFilter(m_drivetrain));
 
     m_vision.setGlobalFilterPipeline(globalPipeline);
     m_vision.setLocalFilteringPipeline(localPipeline);
