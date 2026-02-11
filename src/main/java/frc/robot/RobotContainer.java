39a1e051fd2a27ece85d3a137ef4b6bdb5c19670
riley <rileykjen@gmail.com>
2026-02-10 18:37:31 -0800

    import column

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrainSim;
 import frc.robot.subsystems.drivetrain.DrivetrainCommandFactory;
 import frc.robot.subsystems.intake.IntakeCommandFactory;
 import frc.robot.subsystems.intake.PivotSubsystem;
+import frc.robot.subsystems.intake.ColumnSubsystem;
 import frc.robot.subsystems.intake.IndexerSubsystem;
 import frc.robot.subsystems.scorer.ScorerCommandFactory;
 import frc.robot.subsystems.vision.LocalVisionFilterPipeline;
