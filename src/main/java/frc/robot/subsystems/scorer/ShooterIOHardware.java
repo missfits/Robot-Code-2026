738d19187907e37b4aec491a8f4933b883324dbf
zoe <zoe.z.zhao@gmail.com>
2026-02-22 13:15:21 -0800

    update shootermotortype

--- src/main/java/frc/robot/subsystems/scorer/ShooterIOHardware.java
+++ src/main/java/frc/robot/subsystems/scorer/ShooterIOHardware.java
@@ -2,8 +2,6 @@ package frc.robot.subsystems.scorer;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
-import frc.robot.Constants.ScorerConstants;
-
 import frc.robot.subsystems.MechanismsIOHardwareBase;
 
 public class ShooterIOHardware extends MechanismsIOHardwareBase {
@@ -11,63 +9,27 @@ public class ShooterIOHardware extends MechanismsIOHardwareBase {
   private final ShooterMotorType type;
 
   public ShooterIOHardware(ShooterMotorType type) {
-    super(getMotorId(type), getStatorLimit(type), getLogPrefix(type));
+    super(type.id, type.statorLimit, type.logPrefix);
     this.type = type;
     resetSlot0Gains();
-  }
-
-  private static int getMotorId(ShooterMotorType type) {
-    return switch (type) {
-      case INFLUENCER -> ScorerConstants.INFLUENCER_MOTOR_ID;
-      case FOLLOWER   -> ScorerConstants.FOLLOWER_MOTOR_ID;
-    };
-  }
-
-  private static int getStatorLimit(ShooterMotorType type){
-    return switch (type) {
-      case INFLUENCER -> ScorerConstants.INFLUENCER_MOTOR_STATOR_LIMIT;
-      case FOLLOWER   -> ScorerConstants.FOLLOWER_MOTOR_STATOR_LIMIT;
-    };
-  }
-
-  private static String getLogPrefix(ShooterMotorType type) {
-    return switch (type) {
-      case INFLUENCER -> "shooter/influencer/";
-      case FOLLOWER   -> "shooter/follower/";
-    };
   }
 
   public void resetSlot0Gains() {
     var configs = new TalonFXConfiguration();
     var slot0 = configs.Slot0;
 
-    switch (type) {
-      case INFLUENCER -> {
-        slot0.kP = ScorerConstants.INFLUENCER_kP;
-        slot0.kI = ScorerConstants.INFLUENCER_kI;
-        slot0.kD = ScorerConstants.INFLUENCER_kD;
-        slot0.kS = ScorerConstants.INFLUENCER_kS;
-        slot0.kV = ScorerConstants.INFLUENCER_kV;
-        slot0.kA = ScorerConstants.INFLUENCER_kA;
-      }
-      case FOLLOWER -> {
-        slot0.kP = ScorerConstants.FOLLOWER_kP;
-        slot0.kI = ScorerConstants.FOLLOWER_kI;
-        slot0.kD = ScorerConstants.FOLLOWER_kD;
-        slot0.kS = ScorerConstants.FOLLOWER_kS;
-        slot0.kV = ScorerConstants.FOLLOWER_kV;
-        slot0.kA = ScorerConstants.FOLLOWER_kA;
-      }
-    }
+    slot0.kP = type.gains.kP();
+    slot0.kI = type.gains.kI();
+    slot0.kD = type.gains.kD();
+    slot0.kS = type.gains.kS();
+    slot0.kV = type.gains.kV();
+    slot0.kA = type.gains.kA();
 
     motor.getConfigurator().apply(configs);
   }
 
   private double getDegreesPerRevolution() {
-    return switch (type) {
-      case INFLUENCER -> ScorerConstants.INFLUENCER_DEGREES_PER_REVOLUTION;
-      case FOLLOWER   -> ScorerConstants.FOLLOWER_DEGREES_PER_REVOLUTION;
-    };
+    return type.degreesPerRevolution;
   }
 
   public double getPositionRadians() {
--- src/main/java/frc/robot/subsystems/scorer/ShooterMotorType.java
+++ src/main/java/frc/robot/subsystems/scorer/ShooterMotorType.java
@@ -1,6 +1,53 @@
 package frc.robot.subsystems.scorer;
 
+import frc.robot.Constants.ScorerConstants;
+
 public enum ShooterMotorType {
-  INFLUENCER,
-  FOLLOWER
+  INFLUENCER (
+      ScorerConstants.INFLUENCER_MOTOR_ID,
+      ScorerConstants.INFLUENCER_MOTOR_STATOR_LIMIT,
+      "shooter/influencer/",
+      new Gains(
+          ScorerConstants.INFLUENCER_kP,
+          ScorerConstants.INFLUENCER_kI,
+          ScorerConstants.INFLUENCER_kD,
+          ScorerConstants.INFLUENCER_kS,
+          ScorerConstants.INFLUENCER_kV,
+          ScorerConstants.INFLUENCER_kA
+      ),
+      ScorerConstants.INFLUENCER_DEGREES_PER_REVOLUTION
+  ),
+  FOLLOWER (
+      ScorerConstants.FOLLOWER_MOTOR_ID,
+      ScorerConstants.FOLLOWER_MOTOR_STATOR_LIMIT,
+      "shooter/follower/",
+      new Gains(
+          ScorerConstants.FOLLOWER_kP,
+          ScorerConstants.FOLLOWER_kI,
+          ScorerConstants.FOLLOWER_kD,
+          ScorerConstants.FOLLOWER_kS,
+          ScorerConstants.FOLLOWER_kV,
+          ScorerConstants.FOLLOWER_kA
+      ),
+      ScorerConstants.FOLLOWER_DEGREES_PER_REVOLUTION
+  );
+
+  public final int id;
+  public final int statorLimit;
+  public final String logPrefix;
+  public final Gains gains;
+  public final double degreesPerRevolution;
+
+  ShooterMotorType(int id, int statorLimit, String logPrefix, Gains gains, double degreesPerRevolution) {
+    this.id = id;
+    this.statorLimit = statorLimit;
+    this.logPrefix = logPrefix;
+    this.gains = gains;
+    this.degreesPerRevolution = degreesPerRevolution;
+  }
+
+  /**
+   * Record to group PID and feedforward gains.
+   */
+  public record Gains(double kP, double kI, double kD, double kS, double kV, double kA) {}
 }
