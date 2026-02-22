3149583017be35a2e28ab22e82deff628c286847
aishahnazar <aishahnazar2@gmail.com>
2026-02-22 12:17:53 -0800

    add signal logging

--- src/main/java/frc/robot/subsystems/scorer/ShooterInfluencerIOHardware.java
+++ src/main/java/frc/robot/subsystems/scorer/ShooterInfluencerIOHardware.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.scorer;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+
 import frc.robot.Constants.ShooterConstants;
 
 import frc.robot.subsystems.MechanismsIOHardwareBase;
@@ -11,6 +13,7 @@ public class ShooterInfluencerIOHardware extends MechanismsIOHardwareBase {
   public ShooterInfluencerIOHardware(int motorID) {
     super(motorID, ShooterConstants.INFLUENCER_MOTOR_STATOR_LIMIT,
         ShooterConstants.PEAK_FORWARD_DUTY_CYCLE, ShooterConstants.PEAK_REVERSE_DUTY_CYCLE, "shooterIO/");
+        BaseStatusSignal.setUpdateFrequencyForAll(200, positionSignal, velocitySignal, voltageSignal, currentSignal);
     resetSlot0Gains();
   }
 
