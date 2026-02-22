9abdab34ccd23e150b24d0a62938cb0f6e51f540
aishahnazar <aishahnazar2@gmail.com>
2026-02-22 14:16:15 -0800

    update mechanism structure (#55)
    * create enum for multiple shooter motors
    
    * update shootermotortype
    
    * merge influencer and follower degrees per rev. constant
    
    * use shooterconstants (not scorerconstants)
    
    * format the super() call
    
    * add back setupdatefrequency
    
    * add back MotorOutput peak duty cycle
    
    * allow gains to update during runtime
    
    ---------
    
    Co-authored-by: zoe <zoe.z.zhao@gmail.com>

---
+++ src/main/java/frc/robot/subsystems/scorer/ShooterIOHardware.java
@@ -0,0 +1,71 @@
+package frc.robot.subsystems.scorer;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.MechanismsIOHardwareBase;
+
+public class ShooterIOHardware extends MechanismsIOHardwareBase {
+
+  private final ShooterMotorType type;
+
+  public ShooterIOHardware(ShooterMotorType type) {
+    super(type.id, type.statorLimit, 
+          ShooterConstants.PEAK_FORWARD_DUTY_CYCLE, ShooterConstants.PEAK_REVERSE_DUTY_CYCLE, 
+          type.logPrefix);
+    this.type = type;
+    BaseStatusSignal.setUpdateFrequencyForAll(200, positionSignal, velocitySignal, voltageSignal, currentSignal);
+    resetSlot0Gains();
+  }
+
+  public void resetSlot0Gains() {
+    var configs = new TalonFXConfiguration();
+    var slot0 = configs.Slot0;
+
+    // Get current gains from ShooterConstants to support runtime tuning
+    var gains = type.gains();
+    slot0.kP = gains.kP();
+    slot0.kI = gains.kI();
+    slot0.kD = gains.kD();
+    slot0.kS = gains.kS();
+    slot0.kV = gains.kV();
+    slot0.kA = gains.kA();
+
+    var motorOutputConfigs = configs.MotorOutput;
+    motorOutputConfigs.PeakForwardDutyCycle = ShooterConstants.PEAK_FORWARD_DUTY_CYCLE;
+    motorOutputConfigs.PeakReverseDutyCycle = ShooterConstants.PEAK_REVERSE_DUTY_CYCLE;
+
+    motor.getConfigurator().apply(configs);
+  }
+
+  private double getDegreesPerRevolution() {
+    return ShooterConstants.SHOOTER_DEGREES_PER_REVOLUTION;
+  }
+
+  public double getPositionRadians() {
+    return Math.toRadians(getPositionRevolutions() * getDegreesPerRevolution());
+  }
+
+  public double getPositionDegrees() {
+    return getPositionRevolutions() * getDegreesPerRevolution();
+  }
+
+  public double getVelocityRadiansPerSecond() {
+    return Math.toRadians(getMotorVelocityRevolutionsPerSecond() * getDegreesPerRevolution());
+  }
+
+  public double getVelocityDegreesPerSecond() {
+    return getMotorVelocityRevolutionsPerSecond() * getDegreesPerRevolution();
+  }
+
+  public void setPositionRadians(double radians) {
+    double revolutions = Math.toDegrees(radians) / getDegreesPerRevolution();
+    setPositionRevolutions(revolutions);
+  }
+
+  public void setPositionDegrees(double degrees) {
+    double revolutions = degrees / getDegreesPerRevolution();
+    setPositionRevolutions(revolutions);
+  }
+}
--- src/main/java/frc/robot/subsystems/scorer/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/scorer/ShooterSubsystem.java
@@ -9,8 +9,8 @@ import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.MechanismsSubsystemBase;
 
 public class ShooterSubsystem extends MechanismsSubsystemBase {
-  private final ShooterInfluencerIOHardware m_influencerIO = new ShooterInfluencerIOHardware(ShooterConstants.INFLUENCER_MOTOR_ID);
-  private final ShooterFollowerIOHardware m_followerIO = new ShooterFollowerIOHardware(ShooterConstants.FOLLOWER_MOTOR_ID);
+  private final ShooterIOHardware m_influencerIO = new ShooterIOHardware(ShooterMotorType.INFLUENCER);
+  private final ShooterIOHardware m_followerIO = new ShooterIOHardware(ShooterMotorType.FOLLOWER);
 
   public ShooterSubsystem() {
     super("shooter");
