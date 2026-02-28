d357c5d63a3a9c3b78750a73a2c0af20eada447d
riley <rileykjen@gmail.com>
2026-02-28 15:56:47 -0800

    remove clamp stuff
    now in intake tuning branch

--- src/main/java/frc/robot/subsystems/MechanismsIOHardwareBase.java
+++ src/main/java/frc/robot/subsystems/MechanismsIOHardwareBase.java
@@ -16,7 +16,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
@@ -99,35 +98,6 @@ public abstract class MechanismsIOHardwareBase {
   public void setVelocityVoltage(VelocityVoltage request) {
     targetVelocity = request.Velocity;
     motor.setControl(request);
-  }
-
-  /**
-   * Clamps a value with position-based constraints.
-   *
-   * @param value the value to clamp
-   * @param currentPosition the current position
-   * @param minPosition the minimum allowed position
-   * @param maxPosition the maximum allowed position
-   * @param minValue the minimum value when position is within bounds
-   * @param maxValue the maximum value when position is within bounds
-   * @param bypassMinValue the value to allow when position is below minimum
-   * @param bypassMaxValue the value to allow when position is above maximum
-   * @return the clamped value
-   */
-  public double clamp(double value, double currentPosition, double minPosition, double maxPosition,
-                      double minValue, double maxValue, double bypassMinValue, double bypassMaxValue) {
-    double clampedValue = MathUtil.clamp(value, minValue, maxValue);
-
-    // if position is too low, only allow upward movement
-    if (currentPosition < minPosition) {
-      clampedValue = MathUtil.clamp(clampedValue, bypassMinValue, maxValue);
-    }
-    // if position is too high, only allow downward movement
-    if (currentPosition > maxPosition) {
-      clampedValue = MathUtil.clamp(clampedValue, minValue, bypassMaxValue);
-    }
-
-    return clampedValue;
   }
 
   public void logOutputs() {
