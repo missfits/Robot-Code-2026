fce341350683dcef5c502aa94cc5aa537c213d35
zoe <zoe.z.zhao@gmail.com>
2026-03-09 18:41:45 -0700

    increase motor current

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -161,7 +161,7 @@ public final class Constants {
     public static final int MOTOR_ID = 20;
 
     // Motor limits
-    public static final int MOTOR_STATOR_LIMIT = 40;
+    public static final int MOTOR_STATOR_LIMIT = 60;
 
     // Duty cycle limits
     public static final double PEAK_FORWARD_DUTY_CYCLE = 1;
