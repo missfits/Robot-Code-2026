34ddd1f26ae1e8fbc0bc934f9de148128113ff0d
zoe <zoe.z.zhao@gmail.com>
2026-03-11 16:00:04 -0700

    edit back camera yaw

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -536,14 +536,14 @@ public final class Constants {
           ROBOT_TO_CAM1_Z = Units.inchesToMeters(8.3);
           ROBOT_TO_CAM1_ROLL = 0;
           ROBOT_TO_CAM1_PITCH = Units.degreesToRadians(-20);
-          ROBOT_TO_CAM1_YAW = Units.degreesToRadians(135)-0.12;
+          ROBOT_TO_CAM1_YAW = Units.degreesToRadians(135)-0.12-0.02;
 
           ROBOT_TO_CAM2_X = Units.inchesToMeters(-21.0/2+2.25);
           ROBOT_TO_CAM2_Y = Units.inchesToMeters(33.0/2-6.5);
           ROBOT_TO_CAM2_Z = Units.inchesToMeters(8.5);
           ROBOT_TO_CAM2_ROLL = 0;
           ROBOT_TO_CAM2_PITCH = Units.degreesToRadians(-20);
-          ROBOT_TO_CAM2_YAW = Units.degreesToRadians(-135)+0.14;
+          ROBOT_TO_CAM2_YAW = Units.degreesToRadians(-135)+0.14-0.01;
  
           ROBOT_TO_CAM3_X = Units.inchesToMeters(21/2-0.75); 
           ROBOT_TO_CAM3_Y = Units.inchesToMeters(-33.0/2+2); 
