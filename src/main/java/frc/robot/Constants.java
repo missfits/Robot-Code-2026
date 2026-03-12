04c6bf5793fc8af7fdd78a5491d2ce0e419a6ee9
zoe <zoe.z.zhao@gmail.com>
2026-03-11 17:41:09 -0700

    fix deploy position

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -189,7 +189,7 @@ public final class Constants {
     public static double STORE_POSITION_DEGREES = 10;
     public static double DISPLACE_FUEL_POSITION_DEGREES = 25;
     public static double DEPLOY_POSITION_DEGREES = 55;
-    public static double RESET_DEPLOY_POSITION_DEGREES = DEPLOY_POSITION_DEGREES + 10;
+    public static double RESET_DEPLOY_POSITION_DEGREES = DEPLOY_POSITION_DEGREES + 0;
 
 
     // Timing values for displace fuel command 
