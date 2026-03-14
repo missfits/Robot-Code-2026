e5f6256071404999eb2236623037919df20749aa
zoe <zoe.z.zhao@gmail.com>
2026-03-14 11:46:23 -0700

    lower outtake shooter voltage?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -414,7 +414,7 @@ public final class Constants {
 
     public static double RECYCLE_VELOCITY = 10.0;
     public static double SHUTTLE_VELOCITY = 50.0;
-    public static double OUTTAKE_VOLTAGE = -5.0;
+    public static double OUTTAKE_VOLTAGE = -3.0;
     public static double INTIAL_ADDITIONAL_VELOCITY = 10.0; // TODO: tune
 
     public static double SHOOTER_VOLTAGE = 1.0;
